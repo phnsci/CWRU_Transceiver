2b08e39cd1a1fc5d7fe56330686f6880e4d8fb3a
phn10 <phn10@case.edu>
2019-03-26 17:50:31 -0400

    Phong made edits

--- CWRU_Transceiver_TX/src/CWRU_Transceiver_TX.v
+++ CWRU_Transceiver_TX/src/CWRU_Transceiver_TX.v
@@ -1,10 +1,13 @@
-module CWRU_Transceiver_TX(input clk_50, output [35:0] GPIO_1);
+module CWRU_Transceiver_TX(input CLOCK_50, output [35:0] GPIO_1);
 
 wire clk_4;
 
 assign GPIO_1[17] = clk_4;
+assign GPIO_1[15] = CLOCK_50;
 
-clk_4_gen c (clk_50, clk_4);
+
+
+clk_4_gen c (CLOCK_50, clk_4);
 //button_input b (clk_4, code);
 //LED_display d (clk_4, code);
 
--- CWRU_Transceiver_TX/src/clk_4_gen.v
+++ CWRU_Transceiver_TX/src/clk_4_gen.v
@@ -1,19 +1,19 @@
 module clk_4_gen(input clk_in, output reg clk_out);
 
-reg counter = 'h0000; // for reference to 50MHz cycles, resets at 0x30D4 = 12,500
+reg counter = 16'h0000; // for reference to 50MHz cycles, resets at 0x30D4 = 12,500
 
 initial
 begin
-	clk_out <= 0; // 4kHz clock cycle for binary code transmission
+	clk_out <= 1'b0; // 4kHz clock cycle for binary code transmission
 end
 
 always @(posedge clk_in)
 begin
-	counter <= counter + 1;
-	if (counter == 'h30D4)
+	counter <= counter + 1'b1;
+	if (counter == 16'h30D4)
 	begin
 		clk_out <= ~clk_out;
-		counter <= 'h0000;
+		counter <= 16'h0000;
 	end	
 end
 
